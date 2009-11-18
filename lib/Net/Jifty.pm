65cdbb2cd2ddfc462c1edf02aed6dab7361c2a33
Shawn M Moore <sartak@bestpractical.com>
2009-11-18 18:08:57 -0500

    Document what the config file looks like, why not

--- lib/Net/Jifty.pm
+++ lib/Net/Jifty.pm
@@ -947,6 +947,13 @@ This transforms the config file into a hashref. It also does any postprocessing
 needed, such as transforming localhost to 127.0.0.1 (due to an obscure bug,
 probably in HTTP::Cookies).
 
+The config file is a L<YAML> document that looks like:
+
+    ---
+    email: you@example.com
+    password: drowssap
+    sid: 11111111111111111111111111111111
+
 =head3 write_config_file
 
 This will write the config to disk. This is usually only done when a sid is
